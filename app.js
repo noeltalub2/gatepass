fb548e9b8b73dc341d686359a19ca69263027e5d
noeltalub2 <77842195+noeltalub2@users.noreply.github.com>
2022-12-10 01:27:58 +0800

    Update app.js

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ app.use("/", home);
 // Home Page
 app.use(home);
 
-port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
 	console.log("Server is running");
 });

c7169294322cf3a66def953fb03fd49fad7a6f5e
Noel <jmtalub15@gmail.com>
2022-12-10 10:45:16 +0800

    modified:   .gitignore 	modified:   app.js

--- app.js
+++ app.js
@@ -6,13 +6,12 @@ const cookieSession = require("cookie-session");
 const path = require("path");
 
 const app = express();
-const port = process.env.PORT || 3000;
+
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 //
-const dotenv = require("dotenv");
-dotenv.config();
+
 
 //Routes
 const student = require("./routes/student");
@@ -86,6 +85,6 @@ app.use("/", home);
 app.use(home);
 
 
-app.listen(port, () => {
+app.listen(process.env.PORT || 3000, () => {
 	console.log("Server is running");
 });
